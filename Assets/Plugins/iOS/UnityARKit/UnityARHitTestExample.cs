0b6812baee2f3dd881e675c8dc5f617534e885df
Sha Senevirathne <sha.senevirathne@ofx.com>
2017-07-04 22:59:51 +1000

    Zombie walk, look at and change size

--- Assets/Plugins/iOS/UnityARKit/UnityARHitTestExample.cs
+++ Assets/Plugins/iOS/UnityARKit/UnityARHitTestExample.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using UnityEngine.EventSystems;
 
 namespace UnityEngine.XR.iOS
 {
@@ -28,8 +29,10 @@ namespace UnityEngine.XR.iOS
 			{
 				var touch = Input.GetTouch(0);
 
-				if (touch.phase == TouchPhase.Began)
+				if (touch.phase == TouchPhase.Began && !EventSystem.current.IsPointerOverGameObject(0))
 				{
+					transform.localPosition = Vector3.zero;
+
 					var screenPosition = Camera.main.ScreenToViewportPoint(touch.position);
 					ARPoint point = new ARPoint {
 						x = screenPosition.x,
