73f768ef7ad62625561e548864a53e4f30dcb726
Shanaka Senevirathne <shanaka36@gmail.com>
2017-07-22 16:21:40 +1000

    Restructure for multiple scene management

---
+++ Assets/Scripts/UIManagerBase.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManagerBase : MonoBehaviour {
+
+	public void OnGoBack()
+	{
+		SceneManager.LoadScene("Main");
+	}
+
+	public void OnLoadScene(string sceneName)
+	{
+		if (sceneName == "")
+		{
+			// Load the first scene.
+			SceneManager.LoadScene(1);
+		}
+		
+		SceneManager.LoadScene(sceneName);
+	}
+}
